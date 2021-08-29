702a86c551dd6aa01847ad6c1528cdbca01809d8
Mykhailo Kulinich <ihslimn@gmail.com>
2021-08-29 10:40:22 +0300

    UPD

--- jet-smart-filters-age-filter.php
+++ jet-smart-filters-age-filter.php
@@ -19,14 +19,13 @@ if ( ! defined( 'WPINC' ) ) {
 class Jet_Smart_Filters_Age_Filter {
 
 	private $base_mask = 'age_filter::';
-	private $custom_query = null;
 
 	public function __construct() {
 		add_filter( 'jet-smart-filters/query/final-query', array( $this, 'apply_dates_filter' ), -999 );
 	}
 
 	/**
-	 * Check if your variable is presented in the query. If yes - unset it and add advanced query parameters to filter by
+	 * Check if your variable is presented in the query. If yes - replace years number with dates in timestamp
 	 */
 	public function apply_dates_filter( $query ) {
 
@@ -39,46 +38,22 @@ class Jet_Smart_Filters_Age_Filter {
 			if ( false !== strpos( $meta_query['key'], $this->base_mask ) ) {
 
 				$today = strtotime( 'today midnight' );
+
 				$from = DateTime::createFromFormat( 'U', $today );
-				$from->modify('-'.($meta_query['value'][1]+1).'years');
+				$from->modify( sprintf( '-%dyears', $meta_query['value'][1]+1 ) );
 				$from = (int) $from->format('U')-1;
 
 				$to = DateTime::createFromFormat( 'U', $today );
-				$to->modify('-'.$meta_query['value'][0].'years');
+				$to->modify( sprintf( '-%dyears', $meta_query['value'][0] ) );
 				$to = (int) $to->format('U')+1;
 
 				$data = explode( '::', $meta_query['key'] );
 
 				$field = ! empty( $data[1] ) ? $data[1] : false;
 
-				unset( $query['meta_query'][ $index ] );
-
-				if ( $field ) {
-
-					global $wpdb;
-
-					$table   = $wpdb->postmeta;
-
-					$post_ids_query = "SELECT post_id  FROM `$table` WHERE `meta_key` LIKE '$field' AND `meta_value` BETWEEN '$from' AND '$to'";
-
-					$post_ids = $wpdb->get_results( $post_ids_query );
-
-					if ( empty( $post_ids ) ) {
-						$query['post__in'] = array(0);
-					} else {
-
-						$ids = array();
-
-						foreach ( $post_ids as $id ) {
-							$ids[] = $id->post_id;
-						}
-
-						$query['post__in'] = $ids;
-					}
-
-				}
-
-				
+				$query['meta_query'][ $index ]['key'] = $field;
+				$query['meta_query'][ $index ]['value'][1] = $to;
+				$query['meta_query'][ $index ]['value'][0] = $from;
 
 			}
 		}
